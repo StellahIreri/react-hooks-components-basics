d6db4d00382e1f4442732072c55d26f43a5ee03f
StellahIreri <stellahmireri@gmail.com>
2023-05-18 16:16:05 +0300

    dos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from "react";
-import Navbar from "./Navbar";
-import Home from "./Home";
-import About from "./About";
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
+{/* write an <About> component here */ }
+
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <NavBar />
       <Home />
       <About />
+      {/* add the <Home> component here */}
+      {/* add your <About> component here */}
     </div>
   );
 }
--- src/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Navbar() {
-  return (
-    <div>This is the nav bar.</div>
-  );
-}
-
-export default Navbar;
